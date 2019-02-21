cf4e285c022ed4df2acdb6a4808e525913cd7731
Sergioalf <sealfarofa@ittepic.edu.mx>
2019-02-20 18:55:19 -0700

    Segundo commit

--- formulario.php
+++ formulario.php
@@ -3,9 +3,9 @@
     </head>
     <body>
         <form method="post" action="validacion.php">
-            Nombre: <input type="text" name="name" /><br/>
-            Apellido Paterno: <input type="text" name="apellidop" /><br/>
-            Apellido Materno: <input type="text" name="apellidom" /><br/>
+            Escuela: <input type="text" name="name" /><br/>
+            Colonia: <input type="text" name="apellidop" /><br/>
+            Domicilio: <input type="text" name="apellidom" /><br/>
             <input type="submit" value="enviar" />
         </form>
     </body>
